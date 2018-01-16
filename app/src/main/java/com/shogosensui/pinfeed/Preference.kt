b6b8b0e3c9c5f6ff58367531fe57e952292d1958
1000ch <shogo.sensui@gmail.com>
2018-01-16 23:58:03 +0900

    Remove needless getter

--- app/src/main/java/com/shogosensui/pinfeed/Preference.kt
+++ app/src/main/java/com/shogosensui/pinfeed/Preference.kt
@@ -8,28 +8,24 @@ class Preference(context: Context) {
     val p: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
 
     var userId: String = p.getString("userId", "")
-        get() = userId
         set(value) {
             p.edit().putString("userId", value).commit()
             field = value
         }
 
     var password: String = p.getString("password", "")
-        get() = password
         set(value) {
             p.edit().putString("password", value).commit()
             field = value
         }
 
     var apiToken: String = p.getString("apiToken", "")
-        get() = apiToken
         set(value) {
             p.edit().putString("apiToken", value).commit()
             field = value
         }
 
     var secretToken: String = p.getString("secretToken", "")
-        get() = secretToken
         set(value) {
             p.edit().putString("secretToken", value).commit()
             field = value
